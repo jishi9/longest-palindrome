992c496bcedaabc0b8b0ba3fb259420544dacc6d
Wael Al Jishi <wael.aljeshi@ocado.com>
2015-05-10 19:12:41 +0100

    Algorithm to find longest palindrome

---
+++ longest-palindrome.py
@@ -0,0 +1,68 @@
+class LongestPalindrome(object):
+	"""Finds the longest palindrome in a string"""
+	def __init__(self, string):
+		self.string = string
+		self.size = len(string)
+
+	def find_longest_palindrome(self):
+		odd_radius, best_odd_center = max(self.iter_odd_palindromes_per_center())
+		even_radius, best_even_left_center = max(self.iter_even_palindromes_per_left_center())
+
+		odd_size = 2 * odd_radius + 1
+		even_size = 2 * even_radius
+
+		if odd_size > even_size:
+			left = best_odd_center - odd_radius
+			right = best_odd_center + odd_radius
+		else:
+			left = best_even_left_center - even_radius + 1
+			right = best_even_left_center + even_radius
+
+		return (left, right), self.string[left:right + 1]
+
+
+	# Yields (center, radius_around_center)
+	def iter_odd_palindromes_per_center(self):
+		for center in xrange(self.size):
+			yield self.find_longest_odd_palindrome_at_center(center), center
+
+	# Yields (left_center, radius_around_center**)
+	# ** center being in between left_center and right_center
+	def iter_even_palindromes_per_left_center(self):
+		for left_center in xrange(self.size - 1):
+			yield self.find_longest_even_palindrome_at_left_center(left_center), left_center
+
+	def find_longest_odd_palindrome_at_center(self, center):
+		elements_to_the_left = center
+		elements_to_the_right = self.size - center - 1
+		assert elements_to_the_left + 1 + elements_to_the_right == self.size
+		max_radius = min(elements_to_the_left, elements_to_the_right)
+
+		best_radius = 0
+		for radius in xrange(1, max_radius + 1):
+			if self.string[center - radius] == self.string[center + radius]:
+				best_radius = radius
+			else:
+				break
+
+		return best_radius
+
+	def find_longest_even_palindrome_at_left_center(self, left_center):
+			elements_to_the_left = left_center + 1
+			elements_to_the_right = self.size - left_center - 1
+			assert elements_to_the_left + elements_to_the_right == self.size
+			max_radius = min(elements_to_the_left, elements_to_the_right)
+
+			best_radius = 0
+			for radius in xrange(1, max_radius + 1):
+				if self.string[left_center - radius + 1] == self.string[left_center + radius]:
+					best_radius = radius
+				else:
+					break
+
+			return best_radius
+
+
+print LongestPalindrome('abcdefg').find_longest_palindrome()
+print LongestPalindrome('abcba').find_longest_palindrome()
+print LongestPalindrome('ayabaabccbaabax').find_longest_palindrome()
